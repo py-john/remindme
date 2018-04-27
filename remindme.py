13640f9d06ee81a315386a539f9a22c03217473a
John <pyjohn3@gmail.com>
2018-04-27 00:17:58 -0700

    Added remindme.py

---
+++ remindme.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import os
+import json
+import dateparser
+from sys import argv
+from crontab import CronTab
+from datetime import datetime
+
+PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
+JSON_FILE = f'{PACKAGE_DIR}/reminders.json'
+CRON_CMD =  '. /Users/john/.bash_profile; checkreminders' + \
+		    f' >> {PACKAGE_DIR}/crontab.log 2>&1'
+
+
+def argv_is_valid():
+    if len(argv) < 4:
+        return False
+    if 'to' not in argv[2:]:
+        return False
+    time_input, *reminder = ' '.join(argv[1:]).split(' to ')
+    if not (time_input and reminder):
+        return False
+    return True
+
+
+def save_reminder():
+    time_input, *reminder = ' '.join(argv[1:]).split(' to ')
+    reminder_time = dateparser.parse(time_input)
+    if reminder_time < datetime.now():
+        print('Error: reminder time is in the past')
+        raise SystemExit
+    reminders[reminder_time.isoformat()] = ' to '.join(reminder)
+    with open(JSON_FILE, 'w') as f:
+        json.dump(reminders, f, indent=4)
+    set_crontab()
+
+
+def set_crontab():
+    cron = CronTab(user='john')
+    for job in cron:
+        if job.comment == 'check reminders':
+            return None
+    job = cron.new(command=CRON_CMD, comment='check reminders')
+    job.minute.every(1)
+    cron.write()
+
+
+if __name__ == '__main__':
+    if not argv_is_valid():
+        print("Usage: remindme (in time frame/at time) to (reminder)")
+        raise SystemExit
+    try:
+        with open(JSON_FILE, 'r') as f:
+            reminders = json.load(f)
+    except FileNotFoundError:
+        reminders = {}
+    save_reminder()
