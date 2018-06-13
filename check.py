c3d652fd9d1e109dc3352c491a1fe09421433672
py-john <pyjohn3@gmail.com>
2018-06-12 19:47:34 -0700

    Only write to json after change

--- check.py
+++ check.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
+"""check.py:
+
+Check saved reminders for any the need to be sent.
+
+This is the file that cron runs automatically and shouldn't need 
+to be called manually. When a reminder's time has passed it will be
+sent via Twilio and reminders.json updated with the reminder removed.
+If there are no more reminders left the cron job is removed and this
+file will no longer run until a new reminder is made.
+
+"""
 import os
 import json
 from datetime import datetime
@@ -24,18 +34,21 @@ def check_reminders(reminders):
     reminders are sent and deleted.
     """
     sent_reminders = []
+    have_sent = False
     for timestamp, reminder in reminders.items():
         if timestamp < datetime.now().isoformat():
             textmyself(reminder)
             sent_reminders.append(timestamp)
+            have_sent = True
     for r in sent_reminders:
         del reminders[r]
 
     if not reminders:
         remove_cronjob()
 
-    with open(JSON_FILE, 'w') as f:
-        json.dump(reminders, f, indent=4)
+    if have_sent:
+        with open(JSON_FILE, 'w') as f:
+            json.dump(reminders, f, indent=4)
 
 
 def main():
